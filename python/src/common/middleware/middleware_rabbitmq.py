3f4bb334087d24655c6a2add0bd0d31301058437
GMBUST-FIUBA <gbustamante@fi.uba.ar>
2026-03-28 17:38:55 -0300

    Exchange middleware functions now appearing

--- python/src/common/middleware/middleware_rabbitmq.py
+++ python/src/common/middleware/middleware_rabbitmq.py
@@ -75,3 +75,31 @@ class MessageMiddlewareExchangeRabbitMQ(MessageMiddlewareExchange):
     
     def __init__(self, host, exchange_name, routing_keys):
         pass
+
+    #Comienza a escuchar a la cola e invoca a on_message_callback tras
+	#cada mensaje de datos o de control con el cuerpo del mensaje.
+	# on_message_callback tiene como parámetros:
+	# message - El valor tal y como lo recibe el método send de esta clase.
+	# ack - Función que al invocarse realiza ack al mensaje que se está consumiendo.
+	# nack - Función que al invocarse realiza nack al mensaje que se está consumiendo. 
+	#Si se pierde la conexión con el middleware eleva MessageMiddlewareDisconnectedError.
+	#Si ocurre un error interno que no puede resolverse eleva MessageMiddlewareMessageError.
+    def start_consuming(self, on_message_callback):
+        pass
+        
+    #Si se estaba consumiendo desde la cola, se detiene la escucha. Si
+    #no se estaba consumiendo de la cola, no tiene efecto, ni levanta
+    #Si se pierde la conexión con el middleware eleva MessageMiddlewareDisconnectedError.
+    def stop_consuming(self):
+        pass
+        
+    #Envía un mensaje al tópico con el que se inicializó el exchange.
+    #Si se pierde la conexión con el middleware eleva MessageMiddlewareDisconnectedError.
+    #Si ocurre un error interno que no puede resolverse eleva MessageMiddlewareMessageError.
+    def send(self, message):
+        pass
+
+    #Se desconecta del exchange al que estaba conectado.
+    #Si ocurre un error interno que no puede resolverse eleva MessageMiddlewareCloseError.
+    def close(self):
+        pass
