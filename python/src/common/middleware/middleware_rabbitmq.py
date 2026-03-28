7cf29ada2b10379f9b55ea0f74e7b85de0e48f9e
GMBUST-FIUBA <gbustamante@fi.uba.ar>
2026-03-28 11:30:48 -0300

    Queue name arg non existent

--- python/src/common/middleware/middleware_rabbitmq.py
+++ python/src/common/middleware/middleware_rabbitmq.py
@@ -6,7 +6,35 @@ from .middleware import MessageMiddlewareQueue, MessageMiddlewareExchange
 class MessageMiddlewareQueueRabbitMQ(MessageMiddlewareQueue):
 
     def __init__(self, host, queue_name):
-        return pika.BlockingConnection(pika.ConnectionParameters(host=host, queue_name=queue_name))
+        return pika.BlockingConnection(pika.ConnectionParameters(host=host))
+    
+    #Comienza a escuchar a la cola e invoca a on_message_callback tras
+	#cada mensaje de datos o de control con el cuerpo del mensaje.
+	# on_message_callback tiene como parámetros:
+	# message - El valor tal y como lo recibe el método send de esta clase.
+	# ack - Función que al invocarse realiza ack al mensaje que se está consumiendo.
+	# nack - Función que al invocarse realiza nack al mensaje que se está consumiendo. 
+	#Si se pierde la conexión con el middleware eleva MessageMiddlewareDisconnectedError.
+	#Si ocurre un error interno que no puede resolverse eleva MessageMiddlewareMessageError.
+    def start_consuming(self, on_message_callback):
+        pass
+	
+	#Si se estaba consumiendo desde la cola/exchange, se detiene la escucha. Si
+	#no se estaba consumiendo de la cola/exchange, no tiene efecto, ni levanta
+	#Si se pierde la conexión con el middleware eleva MessageMiddlewareDisconnectedError.
+    def stop_consuming(self):
+        pass
+        
+    #Envía un mensaje a la cola o al tópico con el que se inicializó el exchange.
+    #Si se pierde la conexión con el middleware eleva MessageMiddlewareDisconnectedError.
+    #Si ocurre un error interno que no puede resolverse eleva MessageMiddlewareMessageError.
+    def send(self, message):
+        pass
+
+    #Se desconecta de la cola o exchange al que estaba conectado.
+    #Si ocurre un error interno que no puede resolverse eleva MessageMiddlewareCloseError.
+    def close(self):
+        pass
 
 class MessageMiddlewareExchangeRabbitMQ(MessageMiddlewareExchange):
     
